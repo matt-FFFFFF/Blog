97cb02067c968b772565d1902b1213002e520e2c
Matt White <matt.ffffff@gmail.com>
2019-08-21 16:56:10 +0100

    Reorg repo

--- docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-UID=$(id -u)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-HUGO_VERSION=$(cat $DIR/HUGO_VERSION)
-
-docker run --rm -it -u $UID:$UID -v /home/matt/source/repos/blog:/src -v /home/matt/source/repos/blog/output:/target klakegg/hugo:$HUGO_VERSION-ext
--- website/docker-build.sh
+++ website/docker-build.sh
@@ -4,4 +4,4 @@ UID=$(id -u)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 HUGO_VERSION=$(cat $DIR/HUGO_VERSION)
 
-docker run --rm -it -u 1000:1000 -v /home/matt/source/repos/blog:/src klakegg/hugo:$HUGO_VERSION-ext "new post/$1/index.md"
+docker run --rm -it -u $UID:$UID -v $DIR:/src -v $DIR/build:/target klakegg/hugo:$HUGO_VERSION-ext
