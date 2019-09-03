6afadb0177df9b57b52084b0262d59f934746bd6
Matt White <matt.ffffff@gmail.com>
2019-09-03 16:52:00 +0100

    Save token to temp

--- certbot/issuecert.ps1
+++ certbot/issuecert.ps1
@@ -2,6 +2,8 @@ $domain = 'mattwhite.blog'
 
 Set-PAServer $Env:LE_ENV
 
+$token = Get-Content $Env:AGENT_TEMP/token
+
 # Get a new certificate or submit a renewal. Renewals not yet supported due to token expiry.
 # TO implement renewals, wil lneed to modify token inside plugindata.xml
 New-PACertificate $domain `
@@ -9,8 +11,8 @@ New-PACertificate $domain `
             -Contact 'matt.white@microsoft.com' `
             -DnsPlugin Azure `
             -PluginArgs @{
-                AZSubscriptionId=$($Env:AZURE_SUBSCRIPTION_ID);
-                AZAccessToken=$($Env:AZURE_TOKEN)
+                AZSubscriptionId=$Env:AZURE_SUBSCRIPTION_ID;
+                AZAccessToken=$token
             }
                     
 $cert = Get-PACertificate -MainDomain $domain
