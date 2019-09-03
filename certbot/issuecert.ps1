87478f4f393731577b8fcb281535b24a008106e7
Matt White <matt.ffffff@gmail.com>
2019-09-03 16:15:35 +0100

    Add certbot stuff

---
+++ certbot/issuecert.ps1
@@ -0,0 +1,25 @@
+$domain = 'mattwhite.blog'
+
+
+Set-PAServer LE_PRD
+
+# Get a new certificate or submit a renewal. Renewals not yet supported due to token expiry.
+# TO implement renewals, wil lneed to modify token inside plugindata.xml
+switch ([Boolean](Get-PACertificate -MainDomain $domain)) {
+    $true {
+        New-PACertificate $domain `
+                  -AcceptTOS `
+                  -Contact 'matt.white@microsoft.com' `
+                  -DnsPlugin Azure `
+                  -PluginArgs @{
+                    AZSubscriptionId=$((Get-AzContext).Subscription);
+                    AZAccessToken=$($Env:AZURE_TOKEN)
+                    }
+    }
+    $false {
+        Submit-Renewal -MainDomain $domain
+    }
+}
+
+$cert = Get-PACertificate -MainDomain $domain
+
--- website/docker-build.sh
+++ website/docker-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-UID=$(id -u)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 HUGO_VERSION=$(cat $DIR/HUGO_VERSION)
 
