91857f8394ffb9317acac097b0b1fc74c25f84d2
Matt White <matt.ffffff@gmail.com>
2019-09-03 16:36:46 +0100

    Only issue new certs

--- certbot/issuecert.ps1
+++ certbot/issuecert.ps1
@@ -4,20 +4,12 @@ Set-PAServer LE_PROD
 
 # Get a new certificate or submit a renewal. Renewals not yet supported due to token expiry.
 # TO implement renewals, wil lneed to modify token inside plugindata.xml
-switch ([Boolean](Get-PACertificate -MainDomain $domain)) {
-    $true {
-        New-PACertificate $domain `
-                  -AcceptTOS `
-                  -Contact 'matt.white@microsoft.com' `
-                  -DnsPlugin Azure `
-                  -PluginArgs @{
-                    AZSubscriptionId=$((Get-AzContext).Subscription);
-                    AZAccessToken=$($Env:AZURE_TOKEN)
-                    }
-    }
-    $false {
-        Submit-Renewal -MainDomain $domain
-    }
-}
-
+New-PACertificate $domain `
+            -AcceptTOS `
+            -Contact 'matt.white@microsoft.com' `
+            -DnsPlugin Azure `
+            -PluginArgs @{
+            AZSubscriptionId=$((Get-AzContext).Subscription);
+            AZAccessToken=$($Env:AZURE_TOKEN)
+                    
 $cert = Get-PACertificate -MainDomain $domain
