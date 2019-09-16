bd0fcf809bacaeb326411e57e7838a7e774c46ef
Matt White <matt.ffffff@gmail.com>
2019-09-16 17:02:27 +0100

    gpgyaaaaay!

--- certbot/issuecert.ps1
+++ certbot/issuecert.ps1
@@ -3,8 +3,18 @@ $domain = 'mattwhite.blog'
 Set-PAServer $Env:LE_ENV
 
 # Get a new certificate or submit a renewal. Renewals not yet supported due to token expiry.
-# TO implement renewals, wil lneed to modify token inside plugindata.xml
-New-PACertificate $domain `
+# To implement renewals, wil lneed to modify token inside plugindata.xml
+# PR approved for Posh-ACME, awaiting new version to implement renewal
+
+switch ($Env:letsencryptoperation) {
+    "renew" {
+        Submit-Renewal -PluginArgs @{
+            AZSubscriptionId=$Env:AZURE_SUBSCRIPTION_ID;
+            AZAccessToken=$Env:AZURE_TOKEN
+        }
+    }
+    "newcert" {
+        New-PACertificate $domain `
             -AcceptTOS `
             -Contact 'matt.white@microsoft.com' `
             -DnsPlugin Azure `
@@ -14,4 +24,6 @@ New-PACertificate $domain `
             } `
             -Verbose
                     
-$cert = Get-PACertificate -MainDomain $domain
+        Get-PACertificate -MainDomain $domain
+    }
+}
