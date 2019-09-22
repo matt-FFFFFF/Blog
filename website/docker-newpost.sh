e7334e55f38e38b97ecb32312a92bd96948ae74f
Matt White <matt.ffffff@gmail.com>
2019-09-22 17:46:42 +0100

    Fix error with certbot pipeline Fix error with newpost

--- website/docker-newpost.sh
+++ website/docker-newpost.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 HUGO_VERSION=$(cat $DIR/HUGO_VERSION)
 
-docker run --rm -it -u 1000:1000 -v $DIR:/src klakegg/hugo:$HUGO_VERSION-ext "new post/$1/index.md"
+docker run --rm -it -u 1000:1000 -v $DIR:/src klakegg/hugo:$HUGO_VERSION-ext "new posts/$1/index.md"
